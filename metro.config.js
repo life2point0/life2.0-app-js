0fc4e42bc95701e68249a979c371dba9578ba0da
Tabrez Ahmed <tabrez@MacBook-Pro.local>
2023-10-12 04:39:57 +0530

    EAS

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,9 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+const defaultConfig = getDefaultConfig(__dirname);
+
+
 module.exports = {
+  ...defaultConfig,
     resolver: {
       assetExts: ['jpg', 'png', 'mp4', 'ttf'], // Add or remove file extensions as needed
     },
